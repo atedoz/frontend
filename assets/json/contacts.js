388162d58641fe2e4cc9e8a7f865ad20049c47e2
putrastyo <me.putrasetyo@gmail.com>
2025-05-17 08:39:47 +0700

    feat: json get

---
+++ assets/json/contacts.js
@@ -0,0 +1,20 @@
+const contacts = [
+   {
+      "id": "1",
+      "name": "Email Address",
+      "icon": "📧",
+      "description": "Sutooristudio@mail.com"
+   },
+   {
+      "id": "2",
+      "name": "Phone Number",
+      "icon": "📞",
+      "description": "+6232456678"
+   },
+   {
+      "id": "3",
+      "name": "Studio Address",
+      "icon": "📍",
+      "description": "6391 Elgin St. Celina, Delaware 10299"
+   }
+]
