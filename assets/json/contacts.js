1d7d1daee3a7c3de3353f9620fa704f9a2284b40
Primaaaaa <primayudhistira426@gmail.com>
2025-06-02 03:21:22 +0700

    Final Push

--- assets/json/contacts.js
+++ assets/json/contacts.js
@@ -1,20 +1,14 @@
 const contacts = [
-   {
-      "id": "1",
-      "name": "Email Address",
-      "icon": "📧",
-      "description": "Sutooristudio@mail.com"
-   },
-   {
-      "id": "2",
-      "name": "Phone Number",
-      "icon": "📞",
-      "description": "+6232456678"
-   },
-   {
-      "id": "3",
-      "name": "Studio Address",
-      "icon": "📍",
-      "description": "6391 Elgin St. Celina, Delaware 10299"
-   }
-]
+  {
+    id: "1",
+    name: "Email Address",
+    icon: "📧",
+    description: "Sutooristudio@mail.com",
+  },
+  {
+    id: "2",
+    name: "Instagram",
+    icon: "<i class='bi bi-instagram'></i>",
+    description: "@sutoori.co",
+  },
+];
